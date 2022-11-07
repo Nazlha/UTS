7af02b572da1c4951a43a637ee39587b563bb386
Nazlha <88198350+Nazlha@users.noreply.github.com>
2022-11-07 17:45:55 +0700

    Create jawaban4.py

---
+++ jawaban4.py
@@ -0,0 +1,10 @@
+print("Nama : Nazlha Noor Aiena Sofhie")
+print("Nim : 20210801281")
+print("Jurusan : Teknik Informatika")
+print("===================================")
+adj = ["Kalimantan", "Sumatra", "Sulawesi"]
+Bagian = ["West", "North", "South"]
+ 
+for x in adj:
+  for y in Bagian:
+    print(x, y)
