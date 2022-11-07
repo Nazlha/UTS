bbebb869320fbf84cc9b63a26b986cbcf27baa76
Nazlha <88198350+Nazlha@users.noreply.github.com>
2022-11-07 17:44:53 +0700

    Create jawaban2.py

---
+++ jawaban2.py
@@ -0,0 +1,11 @@
+print("Nama : Nazlha Noor Aiena Sofhie")
+print("Nim : 20210801281")
+print("Jurusan : Teknik Informatika")
+print("---------------------------------------------")
+print("MENGHITUNG LUAS SEGITIGA")
+
+a = int(input("alas = "))
+b = int(input("tinggi = "))
+c = 0.5*a*b
+
+print("luas segitiga adalah: " + str(c))
